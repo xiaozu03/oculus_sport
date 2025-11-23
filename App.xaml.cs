0796baaa353a8ff470d844a1793aa6084de59258
flowerfirst <athirahinani@gmail.com>
2025-11-23 11:26:51 +0800

    Refactor namespace and add Configure() method Updated the namespace declaration to use proper curly braces. Added a new internal static method `Configure()` to the `App` class, which currently throws a `NotImplementedException`. No changes were made to the `CreateWindow` method.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace oculus_sport
+﻿
+namespace oculus_sport
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+        }
+
+        internal static void Configure()
+        {
+            throw new NotImplementedException();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
